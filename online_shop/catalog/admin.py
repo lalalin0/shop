223815f19e7498577ad5922c2902ff683a25f091
alina <155006953+lalalin0@users.noreply.github.com>
2024-04-16 03:58:27 +0300

    Revert "add catalog admin"

--- online_shop/catalog/admin.py
+++ online_shop/catalog/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
 
-from catalog.models import (Category, Product,
-                            Discount, Seller, Order,
-                            CashBack, Promocode, ProductImage)
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('article', 'name', 'price')
-    search_fields = ('article', 'name', 'category__name')
-
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(Discount)
-admin.site.register(Seller)
-admin.site.register(CashBack)
-admin.site.register(Order)
-admin.site.register(ProductImage)
-admin.site.register(Promocode)
-
+# Register your models here.
